0ac2e7c14b9c42e3755fc2269389a5e624f1eafe
ZengFei <felix.zeng@rock-chips.com>
2018-10-25 15:34:39 +0800

    1.adds rknn server support 2.checks udc exist
    Change-Id: I0facea1eb88e845d102c8e948c23fc88fb83a2ac
    Signed-off-by: ZengFei <felix.zeng@rock-chips.com>

---
+++ rknn/usr/bin/start_rknn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export VIV_VX_ENABLE_SWTILING_PHASE1=1
+export VIV_VX_ENABLE_SWTILING_PHASE2=1
+
+export VIV_VX_DEBUG_LEVEL=0
+export VSI_NN_LOG_LEVEL=0
+export RKNN_SERVER_LOGLEVEL=2
+export RKNN_SERVER_PLUGINS='/usr/lib/npu/rknn/plugins/'
+
+while true
+do
+  start_usb.sh ntb
+  sleep 1
+  rknn_server #>/dev/null 2>&1
+done
---
+++ rknn/usr/bin/start_usb.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+case "$1" in
+adb)
+	umount /sys/kernel/config
+	umount /dev/usb-ffs/adb
+
+	echo "none" > /sys/kernel/config/usb_gadget/rockchip/UDC
+
+	mkdir -p /dev/usb-ffs -m 0770
+	mkdir -p /dev/usb-ffs/adb -m 0770
+	mount -t configfs none /sys/kernel/config
+	mkdir /sys/kernel/config/usb_gadget/rockchip  -m 0770
+	echo 0x2207 > /sys/kernel/config/usb_gadget/rockchip/idVendor
+	# ADD_IDPRODUCT
+	echo 0x0006 > /sys/kernel/config/usb_gadget/rockchip/idProduct
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/strings/0x409   -m 0770
+	echo "0123456789ABCDEF" > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/serialnumber
+	echo "rockchip"  > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/manufacturer
+	echo "rk3xxx"  > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/product
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/configs/b.1  -m 0770
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/configs/b.1/strings/0x409  -m 0770
+	echo 500 > /sys/kernel/config/usb_gadget/rockchip/configs/b.1/MaxPower
+
+	# INIT_FUNCTIONS
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/functions/ffs.adb
+	rm -f /sys/kernel/config/usb_gadget/rockchip/configs/b.1/f1
+	ln -s /sys/kernel/config/usb_gadget/rockchip/functions/ffs.adb /sys/kernel/config/usb_gadget/rockchip/configs/b.1/f1
+
+	# INIT_CONFIG
+	echo "adb" > /sys/kernel/config/usb_gadget/rockchip/configs/b.1/strings/0x409/configuration
+
+	# START_APP_BEFORE_UDC
+	mkdir -p /dev/usb-ffs/adb
+	mount -o uid=2000,gid=2000 -t functionfs adb /dev/usb-ffs/adb
+	export service_adb_tcp_port=5555
+	adbd&
+	sleep 1 &&
+
+	UDC=`ls /sys/class/udc/| awk '{print $1}'`
+	echo $UDC > /sys/kernel/config/usb_gadget/rockchip/UDC
+	# START_APP_AFTER_UDC
+
+	;;
+ntb)
+	killall adbd > /dev/null 2>&1
+
+	umount /sys/kernel/config
+	umount /dev/usb-ffs/ntb
+
+	echo "none" > /sys/kernel/config/usb_gadget/rockchip/UDC
+
+	mkdir -p /dev/usb-ffs -m 0770
+	mkdir -p /dev/usb-ffs/ntb -m 0770
+	mount -t configfs none /sys/kernel/config
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip  -m 0770
+	echo 0x2207 > /sys/kernel/config/usb_gadget/rockchip/idVendor
+	# ADD_IDPRODUCT
+	echo 0x1808 > /sys/kernel/config/usb_gadget/rockchip/idProduct
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/strings/0x409   -m 0770
+	echo "0123456789ABCDEF" > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/serialnumber
+	echo "rockchip"  > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/manufacturer
+	echo "rk3xxx"  > /sys/kernel/config/usb_gadget/rockchip/strings/0x409/product
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/configs/b.1  -m 0770
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/configs/b.1/strings/0x409  -m 0770
+	echo 500 > /sys/kernel/config/usb_gadget/rockchip/configs/b.1/MaxPower
+
+	# INIT_FUNCTIONS
+	mkdir -p /sys/kernel/config/usb_gadget/rockchip/functions/ffs.ntb
+	rm -f /sys/kernel/config/usb_gadget/rockchip/configs/b.1/f1
+	ln -s /sys/kernel/config/usb_gadget/rockchip/functions/ffs.ntb /sys/kernel/config/usb_gadget/rockchip/configs/b.1/f1
+
+	# INIT_CONFIG
+	echo "ntb" > /sys/kernel/config/usb_gadget/rockchip/configs/b.1/strings/0x409/configuration
+
+	# START_APP_BEFORE_UDC
+	mkdir -p /dev/usb-ffs/ntb
+	mount -o uid=2000,gid=2000 -t functionfs ntb /dev/usb-ffs/ntb
+
+	;;
+*)
+	echo "Usage: $0 {adb|ntb}"
+	exit 1
+esac
+
+exit 0
