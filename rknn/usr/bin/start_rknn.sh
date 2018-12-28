3df477d0aa70b1887e9a2b3d23521ee95589d63b
Fei Zeng <felix.zeng@rock-chips.com>
2018-12-28 11:11:49 +0800

    Updates rknn server that merge from unification
    Change-Id: I400f1acf1d447873e6233fed4b7f15472b20c932
    Signed-off-by: Fei Zeng <felix.zeng@rock-chips.com>

--- rknn/usr/bin/start_rknn.sh
+++ rknn/usr/bin/start_rknn.sh
@@ -3,9 +3,6 @@
 export VIV_VX_ENABLE_SWTILING_PHASE1=1
 export VIV_VX_ENABLE_SWTILING_PHASE2=1
 
-export VIV_VX_DEBUG_LEVEL=0
-export VSI_NN_LOG_LEVEL=0
-export RKNN_SERVER_LOGLEVEL=0
 export RKNN_SERVER_PLUGINS='/usr/lib/npu/rknn/plugins/'
 
 while true
